c9d05ad1aec378c5eb1c7a2e0102ee381ae04038
Artemy Tregubenko <artemy.tregubenko@here.com>
2015-03-20 15:26:42 +0100

    applying patch from jbergknoff/cucumber-js

--- spec/cucumber/cli/configuration_spec.js
+++ spec/cucumber/cli/configuration_spec.js
@@ -186,9 +186,11 @@ describe("Cucumber.Cli.Configuration", function () {
     beforeEach(function () {
       astFilter      = createSpyWithStubs("AST filter");
       tagFilterRules = [];
+      partRules      = createSpy("part specs");
       scenarioByLineFilterRules = createSpy("line specs");
       spyOn(Cucumber.Ast, 'Filter').andReturn(astFilter);
       spyOn(configuration, 'getTagAstFilterRules').andReturn(tagFilterRules);
+      spyOn(configuration, 'getModularPartitioningRules').andReturn(partRules);
       spyOn(configuration, 'getSingleScenarioAstFilterRule').andReturn(scenarioByLineFilterRules);
     });
 
@@ -199,7 +201,7 @@ describe("Cucumber.Cli.Configuration", function () {
 
     it("instantiates an AST filter", function () {
       configuration.getAstFilter();
-      expect(Cucumber.Ast.Filter).toHaveBeenCalledWith(tagFilterRules);
+      expect(Cucumber.Ast.Filter).toHaveBeenCalledWith([].concat(tagFilterRules).concat(partRules));
     });
 
     it("returns the AST filter", function () {
